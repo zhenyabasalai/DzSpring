050f4ececc9b500ee28adb39beb0eae7885bb2d8
Zhenya <zh.basalay@gmail.com>
2022-11-16 22:31:11 +0300

    Xml Part

---
+++ src/main/java/Dz/XmlPart/Belarusian.java
@@ -0,0 +1,14 @@
+package Dz.XmlPart;
+
+public class Belarusian {
+
+    private String message;
+
+    public void getMessage() {
+        System.out.println(message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/Dz/XmlPart/English.java
@@ -0,0 +1,13 @@
+package Dz.XmlPart;
+
+public class English {
+    private String message;
+
+    public void getMessage() {
+        System.out.println(message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
